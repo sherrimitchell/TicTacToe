c80cbb0160af79ba8950ceb5db5751d582f5a636
Sherri Mitchell <rsamitchell@att.net>
2015-05-11 18:07:58 -0400

    Updated Tic Tac Toe game. Easy version runs.

--- 05-07/tic_tac_toe.rb
+++ 05-07/tic_tac_toe.rb
@@ -1,137 +1,95 @@
 ## May 7 Homework Assignment
 ## Tic Tac Toe
-## Features:
-## Allow 2 player to enter an 'x' or 'o' marker into a space
-## When a player has entered 3 markers of the same type in a row, end the game
+## The game lets 2 players play tic tac toe from the console
+## You win by getting 3 of the same letters in a row horizontally, vertically, or diagonally
+## A draw happens if the board gets filled without either player getting 3 of the same letters in a row
+## A player chooses which letter they will play with, and they only enter that letter during their turn
+## Each player gets one turn to enter a letter into a space.
+## A space only holds one letter, and after a letter is entered it can't be deleted from that space
+## The next player's turn starts after the previous player has entered their letter into a space.
+## The game ends when one player wins or there is a draw
 
-require 'pry'
+# require 'pry'
 
 # Remove/comment require.pry/ binding.pry before sending file to Github
 
-# Print a board for Tic Tac Toe
-
 # binding.pry
 
-board = [
-  [ "b1", "b2", "b3" ],
-  [ "b4", "b5", "b6" ],
-  [ "b7", "b8", "b9" ]
-]
+board = ["b1", "b2", "b3","b4", "b5", "b6", "b7", "b8", "b9"]
+
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "---+----+----"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "---+----+----"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+
+
+  # display_board board
+  # Test for print_board function: display_board board
+
+# A player may select one Player option: x = Player 1, o = Player 2, or Play against the computer.
+
+def player_move(board)
+  display_board(board)
+  puts "Where would you like to move? Please enter the number of the square:"
+  square = gets.chomp
+  available = board.select { |x| x.is_a? Fixnum }
+  until square =~ /^#{available}$/
+    puts "Sorry. Not a valid square. Please enter a numbered square."
+    square = gets.chomp
+  end
+  square.to_i - 1
+end
+
+# A player needs to have 3 spaces filled in a row
+# Check to see if 3 spaces on the board have the same letter
+
+WINS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]]
+
+def win_game?(board)
+  WINS.any? do |a, b, c|
+    board[a] == board[b] &&  board[b] == board[c]
+  end
+end
+  
+def draw_game?(board)
+  board.all? { |x| x.is_a? String }
+end
+
+def game_over?(board)
+  win_game?(board) || draw_game?(board)
+end
+
+def get_entry
+  puts "Please enter an X or O."
+  entry = gets.chomp
+  until entry =~ /^[xo]$/i
+    puts "Sorry. Invalid entry. You must enter an X or an O. "
+  entry = gets.chomp
+end
+entry.upcase
+end
 
 # binding pry
-  
 
-  def print_board(board)
-    puts " 
-      #{board[0][0]} | #{board[0][1]} | #{board[0][2]}
-      ---+----+----
-      #{board[1][0]} | #{board[1][1]} | #{board[1][2]}
-      ---+----+----
-      #{board[2][0]} | #{board[2][1]} | #{board[2][2]} "
-
+def tic_tac_toe
+  board = (1 .. 9).to_a
+  current_player = get_entry
+  until game_over?(board)
+    puts "Next player. Make a move. '#{current_player}'!"
+    move = player_move(board)
+    board[move] = current_player
+    current_player = current_player == 'X' ? 'O' : 'X'
   end
-  
-  # Test for print_board function: print_board board
-
-# A player may select one Player option: Player 1, Player 2, or Play against the computer.
-def player_option(player)
-  player = ""
-  player = gets.chomp.to_i
-
-  if player_option == 1
-    player == player_1
-  elsif player_option == 2
-    player == player_2
-  else player_option == 3
-    player == 3
-  end
-puts player 
+  puts "Game Over!"
 end
 
-def valid_player?(player)
-is_number = (1..3).include?(player)
-  end
-
-def get_player(player)
-  puts "Please enter a Player option:"
-  puts "Enter 1 for Player 1, Enter 2 for Player 2, or Enter 3 to Play Against the Computer."
- until valid_player?(player)
-  puts "Please enter 1 for Player 1, Enter 2 for Player 2, or Enter 3 to Play Against the Computer."
-  player = gets.chomp.to_i
-end
-  player
-end
-
-def display_board(board)
-  print_board(board)
-end
-
-def start_game(player, board)
-  get_player(player)
-  display_board(board)
-end
-
-def turn_complete(player, turn, entry, board)
-  get_player
-  get_entry
-  turn.each do |letter|
-    if is_letter.include?(letter)
-      print entry
-    else get_entry
-    end
-  
-end
-def row_complete(entry, board)
-  
-  entry.each do |entry|
-    if 
-  
-end
-def game_finished?(entry, board, turn_count)
-  turn_count.zero? || row_complete?(entry, board) || row_complete?
-end
-  
-end
-def valid_entry(entry)
-  is_letter = ("x" || "y").include?(entry.downcase)
-  new_letter = !entry.include?(entry.downcase)
-  is_letter && new_letter
-end
-
-def get_entry(entry, space)
-  turn_count = 9
-  puts "Please enter an X or O:"
-  entry = gets.chomp.to_i
-  puts "Please enter the space number that you would like to enter your selection."
-  space_number = ""
-  space = gets.chomp.to_i
-  until valid_entry?(entry)
-    puts "Please enter an X or O."
-  entry = gets.chomp
-  until game_finished?(entry, space, turn_count)
-    if entry == "x" || board[i][j] == board[0]
-
-        
-
-    entry.each do |entry|
-      board.entry.map! { |entry| }
-
-  end
-end
-entry
-end
-
-def play_game(game)
-  
-  until game_finished?(entry, board, turn_count)
-    game_status(game, entry)
-    entry = get_entry()
-    
-  end
-
-  puts "Guess who's a winner?"
-  puts "YOU ARE!"
-  puts "Tic Tac Toe. Three in a row."
+tic_tac_toe
 
 
 
